07b4f318edb66d401f3a82afb89e14d96ba07212
leo <raphael.leo@surgiqual-institute.com>
2020-03-29 00:27:27 +0100

    - merge service workers...

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -10,3 +10,41 @@ firebase.initializeApp({
   appId: "1:773452206716:web:473dcebd0f7b01cb8c2ff9"
 });
 const messaging = firebase.messaging();
+
+self.addEventListener('notificationclick', (event) => {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === event.notification.data.url && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow(event.notification.data.url);
+  }));
+});
+
+self.onnotificationclick = function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === event.notification.data.url && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow(event.notification.data.url);
+  }));
+};
