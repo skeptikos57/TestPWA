0c348b6b46e8d33137e1261ba5e9aa22055f25ff
leo <raphael.leo@surgiqual-institute.com>
2020-03-28 16:15:15 +0100

    - add button to install pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { MessagingService } from './service/messaging.service';
 
 @Component({
@@ -10,6 +10,9 @@ export class AppComponent implements OnInit {
   title = 'push-notification';
   message;
 
+  deferredPrompt: any;
+  showButton = false;
+
   constructor(private messagingService: MessagingService) { }
 
   ngOnInit() {
@@ -17,5 +20,33 @@ export class AppComponent implements OnInit {
     this.messagingService.requestPermission(userId);
     this.messagingService.receiveMessage();
     this.message = this.messagingService.currentMessage;
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
   }
 }
